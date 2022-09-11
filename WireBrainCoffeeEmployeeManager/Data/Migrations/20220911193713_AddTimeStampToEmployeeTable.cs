0b54e242dd161ca846b43c9ade11cb5f8393bb3a
Ain Mor <ainbmor@gmail.com>
2022-09-11 21:49:08 +0200

    migrations

---
+++ WireBrainCoffeeEmployeeManager/Data/Migrations/20220911193713_AddTimeStampToEmployeeTable.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WireBrainCoffeeEmployeeManager.Data.Migrations
+{
+    public partial class AddTimeStampToEmployeeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Timestamp",
+                table: "Employees",
+                type: "tinyint",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "Employees");
+        }
+    }
+}
